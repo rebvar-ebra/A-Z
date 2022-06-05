448d346d1d49cc626d1bf914289bb1202fcc274e
rebvar-ebra <rebwar.ebrahimi@gmail.com>
2022-06-05 16:21:30 +0200

    last version

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => (
 
     <div className="flex justify-center items-center flex-col mt-5">
       <p className="text-white text-sm text-center">Come join us and hear for the unexpected miracle</p>
-      <p className="text-white text-sm text-center font-medium mt-2">info@mrcryptoma.com</p>
+      <p className="text-white text-sm text-center font-medium mt-2"><a href = "mailto:info@mrcryptoma.com?subject = Feedback&body = Message">info@mrcryptoma.com</a></p>
     </div>
 
     <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 " />
--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -41,7 +41,7 @@ const Welcome = () => {
             Send Crypto <br /> across the world
           </h1>
           <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
-            Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
+            Explore the crypto world. Buy and sell cryptocurrencies easily on Cryptoma.
           </p>
           {!currentAccount && (
             <button
@@ -78,16 +78,16 @@ const Welcome = () => {
           <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
             <div className="flex justify-between flex-col w-full h-full">
               <div className="flex justify-between items-start">
-                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
-                  <SiEthereum fontSize={21} color="#fff" />
+                <div className="w-10 h-10 rounded-full border-2 border-black flex justify-center items-center">
+                  <SiEthereum fontSize={21} color="#000" />
                 </div>
-                <BsInfoCircle fontSize={17} color="#fff" />
+                <BsInfoCircle fontSize={17} color="#000" />
               </div>
               <div>
-                <p className="text-white font-light text-sm">
+                <p className="text-block font-light text-sm">
                   {shortenAddress(currentAccount)}
                 </p>
-                <p className="text-white font-semibold text-lg mt-1">
+                <p className="text-block font-semibold text-lg mt-1">
                   Ethereum
                 </p>
               </div>
