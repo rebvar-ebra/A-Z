7adfbc14b3140382d86fc21e34fd89477f048d74
rebvar-ebra <rebwar.ebrahimi@gmail.com>
2022-06-05 13:56:42 +0200

    hook/test/trans

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import{ TransactionProvider } from './context/TransactionContext'
+import { TransactionsProvider } from './context/TransactionContext'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<TransactionProvider>  
+<TransactionsProvider>  
   <React.StrictMode>
   <App />
   </React.StrictMode>  
-</TransactionProvider>)
+</TransactionsProvider>);
